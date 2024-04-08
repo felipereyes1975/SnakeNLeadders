eb439802a31233fb1c136d71df088aede0a59d4c
felipe reyes <felipe.reyes@zsenie.com>
2024-04-08 16:39:17 -0600

    add dataset

---
+++ src/dataSet/food.js
@@ -0,0 +1,18 @@
+exports.FOOD = {
+    1:{
+        name:"hamburger",
+        price:14.4,
+    },
+    2:{
+        name:"hot-dog",
+        price: 5.5
+    },
+    3:{
+        name:"taco",
+        price:2
+    },
+    4:{
+        name:"aquellota",
+        price:1500
+    }
+}
---
+++ src/dataSet/waitress.js
@@ -0,0 +1,14 @@
+exports.WAITRESS = {
+    1:{
+        name:"jaqueline",
+        edad:21,
+    },
+    2:{
+        name:"joaquin",
+        edad:35
+    },
+    3:{
+        name:"mario",
+        edad:21
+    }
+}
